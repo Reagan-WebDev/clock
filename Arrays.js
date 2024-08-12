67593aba2d32aad575e49d4493256a87aac0d428
Reagan Otieno <oluochreagano@gmail.com>
2024-08-12 12:45:04 +0300

    clock

---
+++ Arrays.js
@@ -0,0 +1,4 @@
+const cars = [{make: "Nissan", model: "Note", cc: 1190},
+                {make: "Daihatsu", model: "Mira", cc: 650},
+                {make: "Toyota", model: "Vitz", cc: 1490}];
+console.log(cars[2].make);
